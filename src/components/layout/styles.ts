50a9893bc79e23a43724021589bd739a3987e3a4
YuriMendess <contato.yurimendess@gmail.com>
2023-02-26 02:30:31 -0300

    :lipstick: feat: create container layout

---
+++ src/components/layout/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    margin: 0 1.5rem;
+    max-width: 968px;
+
+    @media screen and (min-width: 968px) {
+        margin: 0;
+    }
+`;
