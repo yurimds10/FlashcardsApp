74cc0ef4c85561e6972ced90ca29e6a0a4276bcd
YuriMendess <contato.yurimendess@gmail.com>
2023-02-26 02:26:25 -0300

    :lipstick: feat: add themes

---
+++ src/global/themes/theme.ts
@@ -0,0 +1,8 @@
+export const theme = {
+    colors: {
+        primary: "#48319d",
+        secondary: "#1f1d47",
+        backgroundColor: "linear-gradient(to right, #2E335A 0%, #45278B 100%)",
+        textColor: "#FFF",
+    }
+}
