a079bf7ac1254911bfc41c7018f52f330c118dc1
Musashi53 <coder73854@proton.me>
2023-12-14 15:01:07 -0300

    :package: build: add and config tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import { GlobalStyle } from './global/styles/GlobalStyle';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import "./styles/global.css";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <GlobalStyle/>
-    
-    <App />    
-  </BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
