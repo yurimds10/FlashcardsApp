eff6f6916ebb5b8090dff2a8ac0833226eb30535
Musashi53 <coder73854@proton.me>
2023-12-19 14:54:20 -0300

    :fire: remove global styles files

--- src/global/themes/theme.ts
+++
@@ -1,8 +0,0 @@
-export const theme = {
-    colors: {
-        primary: "#48319d",
-        secondary: "#1f1d47",
-        backgroundColor: "linear-gradient(to right, #2E335A 0%, #45278B 100%)",
-        textColor: "#FFF",
-    }
-}
